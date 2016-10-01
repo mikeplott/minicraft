d7b1047e802a6621e4d29c9c8d6d7ffc00567055
michaelplott <mike.plott80@gmail.com>
2016-10-01 11:47:05 -0400

    working on filling the background with a grass sprite that was provided with the assests.

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -7,49 +7,48 @@ import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import javafx.scene.layout.Background;
 import javafx.scene.text.Text;
 import javafx.stage.Screen;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	float x, y, xv, yv, velocity;
+	float x, y, xv, yv, totalTime, bgX, bgY, maxWidth, madHeight;
 	TextureRegion down;
 	TextureRegion up;
 	TextureRegion right;
 	TextureRegion left;
 	TextureRegion stand;
 	TextureRegion grass;
+	TextureRegion upStep;
+	TextureRegion downStep;
+	Animation walkRight;
+	Animation walkLeft;
+	Animation walkUp;
+	Animation walkDown;
 	boolean goRight = true;
 	boolean goLeft = false;
 	boolean goUp = false;
 	boolean goDown = false;
 	boolean isSprinting = false;
-	//boolean xBoundary = false;
-	//boolean yBoundary = false;
-
-	//	Stage setting = new Stage();
-//	Camera camera;
-//	TextureAtlas.AtlasSprite atlas;
-//	Texture background;
-
 	static final int WIDTH = 16;
 	static final int HEIGHT = 16;
 	static final int DRAW_WIDTH = WIDTH * 3;
 	static final int DRAW_HEIGHT = HEIGHT * 3;
 	static final float MAX_VELOCITY = 100f;
 	static final float FRICTION = 0.8f;
-	//static final int MAX_WIDTH = Gdx.graphics.getWidth();
-	//static final int MAX_HEIGHT = Gdx.graphics.getHeight();
-
 	//static final float MAX_WIDTH = Gdx.graphics.getWidth();
 	//static final float MAX_HEIGHT = Gdx.graphics.getHeight();
+
+
 
 	
 	@Override
@@ -58,94 +57,71 @@ public class MyGdxGame extends ApplicationAdapter {
 		img = new Texture("tiles.png");
 		Texture tiles = new Texture("tiles.png");
 		TextureRegion[][] grid = TextureRegion.split(tiles, WIDTH, HEIGHT);
+		TextureRegion[][] grassGrid = TextureRegion.split(tiles, 8, 8);
+		grass = grassGrid[0][0];
 		down = grid[6][0];
 		up = grid[6][1];
 		right = grid[6][3];
 		left = new TextureRegion(right);
-		//left.flip(true, false);
-		grass = grid[0][0];
 		stand = grid[6][2];
-
-
-
-		// test code from http://www.aurelienribon.com/blog/2012/06/tutorial-animated-grass-using-libgdx/
-		//trying to draw grass using the sprite across the screen.
-//		float screenW = Gdx.graphics.getWidth();
-//		float screenH = Gdx.graphics.getHeight();
-//		float w = 1;
-//		float h = w * screenH / screenW;
-//
-//		camera = new OrthographicCamera(w, h);
-//		batch = new SpriteBatch();
-//
-		//atlas = new TextureAtlas.AtlasSprite();
-
-
-
+		upStep = new TextureRegion(up);
+		upStep.flip(true, false);
+		downStep = new TextureRegion(down);
+		downStep.flip(true, false);
+		walkRight = new Animation(0.1f, stand, right);
+		walkLeft = new Animation(0.1f, stand, left);
+		walkUp = new Animation(0.1f, upStep, up);
+		walkDown = new Animation(0.1f, downStep, down);
 	}
 
 	@Override
 	public void render () {
-
-		TextureRegion player;
-
+		totalTime += Gdx.graphics.getDeltaTime();
 		movement();
-		 //background = new TextureRegion(grass, 0, 0, MAX_WIDTH, MAX_HEIGHT);
-		//SpriteBatch spriteBatch = new SpriteBatch();
-		//batch.draw(grass,0,0,MAX_WIDTH,MAX_HEIGHT);
-		//batch.draw(grass, x, y, MAX_WIDTH, MAX_HEIGHT);
-
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		TextureRegion player;
+		Gdx.gl.glClearColor(0, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
 		batch.begin();
-
-
-		if (goRight) {
+//		for (int i = 0; i < maxWidth; i++) {
+//			bgX = i;
+//		}
+//		for (int j = 0; j < madHeight; j++) {
+//			bgY = j;
+//		}
+		//batch.
+		//batch.draw(grass, bgX, bgY, DRAW_WIDTH * 2, DRAW_HEIGHT);
+		if (goRight && xv == 0) {
 			player = right;
 			batch.draw(player, x, y, DRAW_WIDTH, DRAW_HEIGHT);
 		}
-		if (goLeft) {
+		else if (goRight && xv > 0) {
+			player = walkRight.getKeyFrame(totalTime, true);
+			batch.draw(player, x, y, DRAW_WIDTH, DRAW_HEIGHT);
+		}
+		else if (goLeft && xv == 0) {
 			player = left;
 			batch.draw(player, x + DRAW_WIDTH, y, DRAW_WIDTH * -1, DRAW_HEIGHT);
 		}
-		if (goUp) {
+		else if (goLeft && xv < 0) {
+			player = walkLeft.getKeyFrame(totalTime, true);
+			batch.draw(player, x + DRAW_WIDTH, y, DRAW_WIDTH * -1, DRAW_HEIGHT);
+		}
+		else if (goUp && yv == 0) {
 			player = up;
 			batch.draw(player, x, y, DRAW_WIDTH, DRAW_HEIGHT);
 		}
-
-		if (goDown) {
+		else if (goUp && yv > 0) {
+			player = walkUp.getKeyFrame(totalTime, true);
+			batch.draw(player, x, y, DRAW_WIDTH, DRAW_HEIGHT);
+		}
+		else if (goDown && yv == 0) {
 			player = down;
 			batch.draw(player, x, y, DRAW_WIDTH, DRAW_HEIGHT);
 		}
-
-
-
-
-		//batch.draw(grass, MAX_WIDTH, MAX_HEIGHT);
-		//batch.draw(grass, MAX_WIDTH, MAX_HEIGHT);
-//		if (goUp) {
-//			player = up;
-//			//batch.draw(player, x, y, DRAW_WIDTH, DRAW_HEIGHT);
-//		}
-//		else if (!goUp) {
-//			player = down;
-//			//batch.draw(player, x, y, DRAW_WIDTH, DRAW_HEIGHT * -1);
-//		}
-//		else if (goRight) {
-//			player = right;
-//			//batch.draw(player, x, y, DRAW_WIDTH, DRAW_HEIGHT);
-//		}
-//		else if (!goRight) {
-//			player = left;
-//			//batch.draw(player, x, y, DRAW_WIDTH * -1, DRAW_HEIGHT);
-//		}
-//		else {
-//			player = right;
-//			//batch.draw(player, x, y, DRAW_WIDTH, DRAW_HEIGHT);
-//		}
-
-
+		else if (goDown && yv < 0) {
+			player = walkDown.getKeyFrame(totalTime, true);
+			batch.draw(player, x, y, DRAW_WIDTH, DRAW_HEIGHT);
+		}
 		batch.end();
 	}
 	
@@ -168,9 +144,6 @@ public class MyGdxGame extends ApplicationAdapter {
 			else {
 				yv = MAX_VELOCITY * 2;
 			}
-//			if (yv < 0) {
-//				yv =
-//			}
 			goRight = false;
 			goDown = false;
 			goLeft = false;
@@ -214,38 +187,20 @@ public class MyGdxGame extends ApplicationAdapter {
 			goDown = false;
 			goRight = true;
 		}
-
-//		if (x > MAX_WIDTH) {
-//			xBoundary = true;
-//		}
-//		if (x < 0) {
-//			xBoundary = false;
-//		}
-
-
-
 		x += xv * Gdx.graphics.getDeltaTime();
 		y += yv * Gdx.graphics.getDeltaTime();
-		if (x < 0) {
+		if (x < -30) {
 			x = Gdx.graphics.getWidth();
 		}
 		if (x > Gdx.graphics.getWidth()) {
-			x = 0;
+			x = -30;
 		}
-		if (y < 0) {
+		if (y < -30) {
 			y = Gdx.graphics.getHeight();
 		}
 		if (y > Gdx.graphics.getHeight()) {
-			y = 0;
+			y = -30;
 		}
-//
-//		if (x > MAX_WIDTH) {
-//			x = 0;
-//		}
-//		if (x < 0) {
-//			x = MAX_WIDTH;
-//		}
-
 		yv = decelerate(yv);
 		xv = decelerate(xv);
 	}
@@ -258,3 +213,75 @@ public class MyGdxGame extends ApplicationAdapter {
 		return velocity;
 	}
 }
+
+
+
+
+
+// test code from http://www.aurelienribon.com/blog/2012/06/tutorial-animated-grass-using-libgdx/
+//trying to draw grass using the sprite across the screen.
+//		float screenW = Gdx.graphics.getWidth();
+//		float screenH = Gdx.graphics.getHeight();
+//		float w = 1;
+//		float h = w * screenH / screenW;
+//
+//		camera = new OrthographicCamera(w, h);
+//		batch = new SpriteBatch();
+//
+//atlas = new TextureAtlas.AtlasSprite();
+//
+//		if (x > MAX_WIDTH) {
+//			x = 0;
+//		}
+//		if (x < 0) {
+//			x = MAX_WIDTH;
+//		}
+//		if (x > MAX_WIDTH) {
+//			xBoundary = true;
+//		}
+//		if (x < 0) {
+//			xBoundary = false;
+//		}
+//			if (yv < 0) {
+//				yv =
+//			}
+//batch.draw(grass, MAX_WIDTH, MAX_HEIGHT);
+//batch.draw(grass, MAX_WIDTH, MAX_HEIGHT);
+//		if (goUp) {
+//			player = up;
+//			//batch.draw(player, x, y, DRAW_WIDTH, DRAW_HEIGHT);
+//		}
+//		else if (!goUp) {
+//			player = down;
+//			//batch.draw(player, x, y, DRAW_WIDTH, DRAW_HEIGHT * -1);
+//		}
+//		else if (goRight) {
+//			player = right;
+//			//batch.draw(player, x, y, DRAW_WIDTH, DRAW_HEIGHT);
+//		}
+//		else if (!goRight) {
+//			player = left;
+//			//batch.draw(player, x, y, DRAW_WIDTH * -1, DRAW_HEIGHT);
+//		}
+//		else {
+//			player = right;
+//			//batch.draw(player, x, y, DRAW_WIDTH, DRAW_HEIGHT);
+//		}
+//background = new TextureRegion(grass, 0, 0, MAX_WIDTH, MAX_HEIGHT);
+//SpriteBatch spriteBatch = new SpriteBatch();
+//batch.draw(grass,0,0,MAX_WIDTH,MAX_HEIGHT);
+//batch.draw(grass, x, y, MAX_WIDTH, MAX_HEIGHT);
+//static final int MAX_WIDTH = Gdx.graphics.getWidth();
+//static final int MAX_HEIGHT = Gdx.graphics.getHeight();
+
+//static final float MAX_WIDTH = Gdx.graphics.getWidth();
+//static final float MAX_HEIGHT = Gdx.graphics.getHeight();
+//boolean xBoundary = false;
+//boolean yBoundary = false;
+
+//	Stage setting = new Stage();
+//	Camera camera;
+//	TextureAtlas.AtlasSprite atlas;
+//	Texture background;
+//left.flip(true, false);
+
